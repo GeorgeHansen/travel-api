1d22080a6d69423ad76952334d42e1d2bd74c4c5
NicholasHansen <nicholasfhansen@gmail.com>
2015-12-01 12:27:40 +0100

    fixing booking, adding docs for booking

--- models/booking.js
+++ models/booking.js
@@ -6,9 +6,9 @@ var Schema			= mongoose.Schema;
 var bookingSchema = new Schema({
 
 	username: 		{  type: String, required: true},
-	email: 			{  type: Number, min: 0, required: true},
-	fromDate: 		{  type: Date, default: Date.now, required:true},
-	toDate: 		{  type: Date, default: Date.now, required:true},
+	email: 			{  type: String, required: true},
+	fromDate: 		{  type: Date, default: Date.now, required:false},
+	toDate: 		{  type: Date, default: Date.now, required:false},
 
 });
 
--- routes/bookings.js
+++ routes/bookings.js
@@ -7,28 +7,98 @@ var bodyParser = require('body-Parser');
 var router = express.Router();
 var Booking = require('../models/booking');
 
+/**
+ * @api {get} /bookings/ Get all Bookings
+ * @apiName GetAllBookings
+ * @apiGroup Bookings
+ * @apiDescription Is used to get all the bookings currently in the database in JSON format. Just quickly threw something up there to have it. Dates are a little weird atm. You can leave them blank for the current date
+
+ * @apiVersion 0.2.0
+ *
+ * @apiSuccess {ID} id ID of the booking.
+ * @apiSuccess {String} username  Username of the person doing the booking.
+ * @apiSuccess {String} email email address for person doing the booking. 
+ * @apiSuccess {Date} fromDate. Date booking starts from
+ * @apiSuccess {Date} toDate Date booking ends.
+ * 
+ *
+ * @apiSuccessExample {json} Success-Response:
+ *     HTTP/1.1 200 OK
+ * [
+ *   {
+ * 
+ *
+ *  "_id": "565d82e58eaa68c014dc550e",
+ *  "username": "Test",
+ *  "email": "test@test.com",
+ *  "toDate": "2015-12-01T11:22:41.239Z",
+ *  "fromDate": "2015-12-01T11:22:41.239Z"
+ *
+ *   }
+ * ]
+ *
+ *
+ * @apiError (Error 5xx) 500 Internal Server Error 
+ * @apiErrorExample {json} Error-Response:
+ *      HTTP/1.1 500 Internal Server Error
+ *      {
+ *          "message" : "Internal Server Error"
+ *      }
+ */
 router.route('/bookings/')
 	.get(function (req,res)
 	{
-		   Booking.find({},' -__v',function(err, bookings) {
-            if(err) {
-                console.log(err);
-                res.status(500);
-                return res.json({"message" : "Internal Server Error"});
-            }
-            else
-            {
-                res.status(200);
-                return res.json(bookings);
-            }
-        });
+		   Booking.find({},' -__v',function(err, bookings) 
+		   {
+	            if(err) 
+	            {
+	                console.log(err);
+	                res.status(500);
+	                return res.json({"message" : "Internal Server Error"});
+	            }
+	            else
+	            {
+	                res.status(200);
+	                return res.json(bookings);
+	            }
+        	});
 	})
-	.post(function(req, res) {
+	 /**
+     * @api {post} /bookings/ Create Booking
+     * @apiName createBooking
+     * @apiGroup Bookings
+     * @apiDescription Is used to create a new booking. 
+     * Dates are optional. If nothing is entered current date will be used. 
+     *
+     * @apiVersion 0.2.0
+     * @apiParam {String} username          Mandatory userName for booking
+     * @apiParam {String} email             Mandatory userEmail for booking
+     * @apiParam {Date} [fromDate]        	Optional starting date for booking
+     * @apiParam {String} [toDate]     		Optional  ending date for booking
+     * 
+     *      
+     * @apiParamExample {json} Post-Example:
+	 * [
+	 *   {
+	 * 
+	 *
+	 *  "_id": "565d82e58eaa68c014dc550e",
+	 *  "username": "Test",
+	 *  "email": "test@test.com",
+	 *  "toDate": "2015-12-01T11:22:41.239Z",
+	 *  "fromDate": "2015-12-01T11:22:41.239Z"
+	 *
+	 *   }
+	 * ]
+     *   
+     */
+	.post(function(req, res) 
+	{
 
-	    //var country;
-	    console.log(req.body);
+	    var booking;
+	    //console.log(req.body);
 
-	    var booking = new Booking({
+	   booking = new Booking({
 	        username: req.body.username,
 	        email: req.body.email,
 	        fromDate: req.body.fromDate,
@@ -41,7 +111,7 @@ router.route('/bookings/')
 	        if (!err) 
 	        {
 	            res.json({
-	                "message": "booking for " + booking.username + " created"
+	                "message": "booking for"
 	            });
 	            res.status(201);
 	            console.log("booking for " + booking.username + " created");
@@ -55,13 +125,7 @@ router.route('/bookings/')
 		});
 	});
 
-router.route('/booking/:id')
 
-	.get(function(req,res)
-	{
-
-
-	});
 
 
 module.exports = router;
